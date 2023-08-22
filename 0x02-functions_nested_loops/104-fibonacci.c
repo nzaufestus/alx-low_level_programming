18e78a9e08d6682f765480bfcb34de732700609a
nzaufestus <nzaufestus7@gmail.com>
2023-08-22 02:25:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int count;
-unsigned long fib1 = 1, fib2 = 2, sum;
+unsigned long long fib1 = 1, fib2 = 2, sum;
 printf("%lu, %lu, ", fib1, fib2);
 for (count = 3; count <= 98; count++)
 {
